85d9f29038d33667798d0385bc61dfc5c3a3e661
duclongg9 <duclongg9@gmail.com>
2026-02-24 10:46:40 +0700

    update src

---
+++ mvvm_project/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_project/di.dart';
+import 'package:mvvm_project/viewmodels/login/login_viewmodel.dart';
+import 'package:mvvm_project/views/login_page.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<LoginViewModel>(
+      create: (_) => buildLoginVM(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+      ),
+    );
+  }
+}
